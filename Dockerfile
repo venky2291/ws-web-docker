a365ddece721dfbc8c34254494d0e593251001dc
venky2291 <venkyin.2218@gmail.com>
2020-05-31 04:20:05 +0000

    FirstCommit

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM venky2291/apache-webserver-php
+
+COPY index.php  /var/www/html/index.php
---
+++ index.php
@@ -0,0 +1,14 @@
+<body bgcolor='aqua'>
+<pre>
+
+<?php
+
+print "welcome to devops-venky2291 web server for testing";
+
+
+print `ifconfig`;
+
+?>
+
+</pre>
+
